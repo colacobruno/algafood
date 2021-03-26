0a25c755a6e279114a87d822bb7444cc492e7e02
colacobruno <bruno@colaco.com.br>
2021-03-26 09:23:49 -0300

    4.23 - Inclusão cozinhas - POST

---
+++ src/main/java/com/algaworks/algafood/api/controller/CozinhaController.java
@@ -0,0 +1,53 @@
+package com.algaworks.algafood.api.controller;
+
+import com.algaworks.algafood.domain.model.Cozinha;
+import com.algaworks.algafood.domain.model.CozinhasXmlWrapper;
+import com.algaworks.algafood.domain.repository.CozinhaRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import javax.xml.bind.annotation.XmlRootElement;
+import java.util.List;
+
+@XmlRootElement
+@RestController
+@RequestMapping("/cozinhas")
+public class CozinhaController {
+
+    @Autowired
+    private CozinhaRepository cozinhaRepository;
+
+
+    @GetMapping(produces = MediaType.APPLICATION_JSON_VALUE)
+    public List<Cozinha> listar1() {
+        return cozinhaRepository.listar();
+    }
+
+    @GetMapping(produces = MediaType.APPLICATION_XML_VALUE)
+    public CozinhasXmlWrapper listarXml(){
+        return new CozinhasXmlWrapper(cozinhaRepository.listar());
+    }
+
+    @GetMapping("/{cozinhaId}")
+    public ResponseEntity<Cozinha> buscar(@PathVariable("cozinhaId") Long id){
+        Cozinha cozinha = cozinhaRepository.buscar(id);
+
+        if(cozinha != null)
+        {
+            return ResponseEntity.ok(cozinha);
+        }
+
+        // return ResponseEntity.status(HttpStatus.NOT_FOUND).build();
+        return ResponseEntity.notFound().build();
+    }
+
+    @PostMapping()
+    @ResponseStatus(HttpStatus.CREATED)
+    public Cozinha adicionar(@RequestBody Cozinha cozinha){
+        return cozinhaRepository.salvar(cozinha);
+    }
+
+}
---
+++ src/main/java/com/algaworks/algafood/domain/model/CozinhasXmlWrapper.java
@@ -0,0 +1,19 @@
+package com.algaworks.algafood.domain.model;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.dataformat.xml.annotation.JacksonXmlElementWrapper;
+import com.fasterxml.jackson.dataformat.xml.annotation.JacksonXmlRootElement;
+import lombok.Data;
+import lombok.NonNull;
+
+import java.util.List;
+
+@JacksonXmlRootElement(localName = "Cozinhas")
+@Data
+public class CozinhasXmlWrapper {
+
+    @JsonProperty("Cozinha")
+    @JacksonXmlElementWrapper(useWrapping = false)
+    @NonNull
+    private List<Cozinha> cozinhas;
+}
